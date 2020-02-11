c0354d33f1a41f370c49c280009eda4b51fc9361
Georgios Manoltzas <g.manoltzas@indice.gr>
2020-02-12 01:08:24 +0200

    Initial commit for MicroRabbit.Infrastructure.IoC project.

---
+++ src/Infrastructure.IoC/MicroRabbit.Infrastructure.IoC/DependencyContainer.cs
@@ -0,0 +1,13 @@
+﻿using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infrastructure.Bus;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MicroRabbit.Infrastructure.IoC
+{
+    public class DependencyContainer
+    {
+        public static void ConfigureServices(IServiceCollection services) {
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
